213aa965db3981f52e29500a2d4325b400ed855c
Ivan Pinatti <ivan-pinatti@users.noreply.github.com>
2016-12-01 14:05:05 -0500

    Fix echo msg output

--- scripts/run.sh
+++ scripts/run.sh
@@ -54,7 +54,7 @@ if [ $? -eq 0 ]; then
   If you prefer to use URL instead, please add it to your hosts file
   Just copy and paste the command below in your terminal or add it manually later
 
-  sudo bash -c 'echo ${DOCKER_CONTAINER_IP} solr.local >> /etc/hosts'
+  sudo bash -c 'echo "${DOCKER_CONTAINER_IP} solr.local" >> /etc/hosts'
 
   THEN, will be able to access at:
   http://solr.local:${DOCKER_CONTAINER_PORT}
